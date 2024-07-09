b2a6c8a1511aeb0aca2d68961357b23d2249fd08
MarcioLino27 <marciolino027@gmail.com>
2024-07-09 19:08:19 -0300

    Add files via upload

---
+++ JavaSctipt - Aula 03/Atividade 02/script.js
@@ -0,0 +1,13 @@
+for (let i = 1; i <= 10; i++) {
+    
+    if (i % 2 === 0) 
+        {
+        continue;
+        }
+
+    if (i === 8) 
+        {
+        break;
+        }
+    console.log(i);
+}
---
+++ JavaSctipt - Aula 03/Atividade 04/script.js
@@ -0,0 +1,6 @@
+let texto = prompt("Digite uma palavra ou frase:");
+
+for (let caractere of texto) 
+    {
+    console.log(caractere);
+    }
